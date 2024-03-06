e142fccf4ef77d382802a6cecc1fcd76b9095ed8
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-06 15:04:44 -0300

    init 1.3.1.1-cmn.sh

---
+++ Unidade-I/Cap-1/1.3-Estrutura-SFN/1.3-Esturuta-SFN.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+:'
+    1.3-Esturuta Atual do Sistema Financeiro Nacional
+    1.3.1-Orgãos de Regulação e Fiscalização
+    1.3.1.1-autoridades Monetárias
+    1.3.1.1.1-CMN-Conselho Monetários Nacional
+'
---
+++ Unidade-I/Cap-1/1.3-Estrutura-SFN/1.3.1-Fiscal-Regulamentar/1.3.1.1-CMN.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+:'
+    Fonte: Lei 4595/64 -
+        Dispõe sobre a Política e as Instituições Monetárias, Bancárias e Creditícias, Cria o Conselho Monetário Nacional e dá outras providências.
+
+    web: https://www.planalto.gov.br/ccivil_03/leis/l4595compilado.htm
+'
+
+:'
+    Lei n° 4595/64
+        Capítulo II - Do Conselho Monetário Nacional
+
+            Art. 2º Fica extinto o Conselho da atual Superintendência da Moeda e do Crédito, 
+            e criado em substituição, o Conselho Monetário Nacional, 
+            com a finalidade de formular a política da moeda e do crédito como previsto nesta lei, 
+            objetivando o progresso econômico e social do País.
+'
