f37e0fa7157bc1a5f5a9a049c74c23f50c3b115e
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-30 09:54:10 -0300

    Uptade

---
+++ Unidade-I/Cap-1/1.3-Estrutura-SFN/1.3.1-Fiscal-Regulamentar/1.3.1.1-Autoridades-Monetarias/1.3.1.1.1-CMN.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+:' orgão normativo, diretrizes, políticas monetárias, cambial, creditícia.
+política Econômica, Runiões Ordinárias, Extraordinárias comvocadas pelo presidente do CMN,
+
+'
+
+
+:'
+    Fonte: Lei 4595/64 -
+        Dispõe sobre a Política e as Instituições Monetárias, Bancárias e Creditícias, Cria o Conselho Monetário Nacional e dá outras providências.
+
+    web: https://www.planalto.gov.br/ccivil_03/leis/l4595compilado.htm
+'
+
+:'
+    Lei n° 4595/64
+        Capítulo II - Do Conselho Monetário Nacional
+
+            Art. 2º Fica extinto o Conselho da atual Superintendência da Moeda e do Crédito, 
+            e criado em substituição, o Conselho Monetário Nacional, 
+            com a finalidade de formular a política da moeda e do crédito como previsto nesta lei, 
+            objetivando o progresso econômico e social do País.
+'
+
+:'Atribuições do Conselho típicas
+        Lei n° 4595/6
+        a) na qualidade de conselho de política econômica:
+            adaptar o volume dos meios de pagamentos às reias necessidades da economia;
+            regular os valores interno e externo da moeda;
+            coordenar as plíticas monetárias, creditícia, orçamentária, fiscal e da dívida pública;
+            autorizar emissões de papel moeda;
+            aprovar os orçamentos monetários preparados pelo Bacen;
+'
+
+:'Atribuições do Conselho atípicas
+        Lei n° 4595/6
+'
